d0cb2edb8b1aca43503704bd3143a8808058b90e
AJ Deck <ajd4223@rit.edu>
2014-01-14 11:20:00 -0500

    Still working on the speakersClient.

--- speakersClient.py
+++ speakersClient.py
@@ -4,8 +4,10 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+MYCROFT_PORT = 1847                # Reserve a port for your service.
+manifest = open("./app.json")
 
-s.connect((host, port))
+s.connect((host, MYCROFT_PORT))
+s.send(manifest)
 print (s.recv(1024))
-s.close                     # Close the socket when done
+s.close                # Close the socket when done
