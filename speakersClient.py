0f7b41ceffd6ba191ba0fe3a79b11f4724b2ba3c
AJ Deck <ajd4223@rit.edu>
2014-01-14 11:38:19 -0500

    Can connect to Mycroft now but having issues sending the manafest.

--- speakersClient.py
+++ speakersClient.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python           # This is client.py file
 
 import socket               # Import socket module
+import sys
+
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 MYCROFT_PORT = 1847                # Reserve a port for your service.
-manifest = open("./app.json")
-
+manifest = "APP_MANIFEST " + open("./app.json").read()
+size = sys.getsizeof(manifest)
 s.connect((host, MYCROFT_PORT))
-s.send(manifest)
-print (s.recv(1024))
+input("Send manifest?")
+s.send(bytes((str(size)+"\n"+manifest), 'UTF-8'))
+print("Manifest sent. Maybe?")
+print (s.recv(1024)) 
 s.close                # Close the socket when done
