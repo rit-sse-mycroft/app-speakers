062f71d2abb0b253f590a05ddbab5248f7ec69bf
Jesse Jurman <j.r.jurman@gmail.com>
2014-01-20 14:50:22 -0500

    ffplay code

--- speakers.rb
+++ speakers.rb
@@ -1,4 +1,5 @@
 require 'mycroft'
+require 'socket'
 
 class Speakers < Mycroft::Client
 
@@ -8,6 +9,7 @@ class Speakers < Mycroft::Client
     @key = '/path/to/key'
     @cert = '/path/to/cert'
     @manifest = './app.json'
+    @port = 3000
     @verified = false
   end
 
@@ -16,7 +18,20 @@ class Speakers < Mycroft::Client
   end
 
   def on_data(data)
-    # Your code here
+    puts data
+    parsed = parse_message(data)
+    if parsed[:type] == 'APP_MANIFEST_OK' or parsed[:type] == 'APP_MANIFEST_FAIL'
+      check_manifest(parsed)
+      @verified = true
+      `ffplay rtp://127.0.0.1:#{@port}`
+    elsif parsed[:type] == 'MSG_QUERY'
+      #get ip address
+      if parsed[:data]["action"] == "get ip"
+        clientIP = Socket::getaddrinfo(Socket.gethostname,"echo",Socket::AF_INET)[0][3]
+        content = {ip: clientIP, port: @port}
+        query_success(parsed[:data]["id"], content)
+      end
+    end
   end
 
   def on_end
