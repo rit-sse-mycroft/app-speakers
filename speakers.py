39c910bdea3e833ec1177659b58092f5e4ba4b85
kristenmills <kristen@kristen-mills.com>
2014-02-02 22:27:12 -0500

    Rewrote speakers app in python. Added support for streaming spotify

---
+++ speakers.py
@@ -0,0 +1,46 @@
+from mycroft.client import MycroftClient
+import subprocess, pyaudio, socket, threading
+
+def app_dependency(client, msg_type, data):
+    client.up()
+
+def msg_query(client, msg_type, data):
+    if data['action'] == 'stream_tts':
+        client_ip = data['data']['ip']
+        port = data['data']['port']
+        subprocess.call('vlc tcp://{0}:{1} vlc://quit'.format(client_ip, port))
+    elif data['action'] == 'stream_video':
+        subprocess.call('vlc {0} vlc://quit'.format(data['data']))
+    elif data['action'] == 'stream_spotify':
+        client_ip = data['data']['ip']
+        port = data['data']['port']
+        client = socket.create_connection((client_ip, port))
+        thread = threading.Thread(target=play_music, args=[client])
+        thread.start()
+
+def play_music(client):
+    chunk = 2048
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16,
+            channels=2,
+            rate=44100,
+            output=True)
+
+    while True:
+        try:
+            data = client.recv(8192)
+            stream.write(data)
+        except ConnectionResetError:
+            break
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+
+
+client = MycroftClient('localhost', 1847, './app.json')
+client.on('APP_DEPENDENCY', app_dependency)
+client.on('MSG_QUERY', msg_query)
+client.start()
